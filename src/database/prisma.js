f089323349eeb2ba46fb2922ac25ee9a6549725f
Admin User <admin@mylbs.com>
2026-04-27 23:17:22 +0100

    Simplify Prisma client and nixpacks

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,15 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-const prisma = new PrismaClient({
-  log: ['error', 'warn'],
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 export default prisma;
