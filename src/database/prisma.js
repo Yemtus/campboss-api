41d8585d4b9a3179663e605b8e9104e4910d8481
Admin User <admin@mylbs.com>
2026-04-28 10:38:04 +0100

    Temporary hardcode DB URL for testing

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const DATABASE_URL = process.env.DATABASE_URL || 
+  'postgresql://postgres:DfjoTQRvdvzRsGfJqjjmTAeVNVLZtzRw@shuttle.proxy.rlwy.net:52479/railway';
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
